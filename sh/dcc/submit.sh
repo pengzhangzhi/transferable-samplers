0bb7c6506c9c4bf13a3f4a384235b2838e3dfc4f
fred peng <zp70@duke.edu>
2026-01-07 17:23:11 -0500

    Update SLURM job submission script for FastFlow training
    This commit modifies the submit.sh script to reduce GPU allocation from 4 to 2 and extend the job time from 1 day to 2 days. Additionally, it updates the training command to adjust the model's KL loss parameters and changes the student checkpoint path for improved training consistency.

--- sh/dcc/submit.sh
+++ sh/dcc/submit.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=fastflow
 #SBATCH --account=h200ea
 #SBATCH --partition=h200ea
-#SBATCH --gres=gpu:h200:4
-#SBATCH --time=1-00:00:00
+#SBATCH --gres=gpu:h200:2
+#SBATCH --time=2-00:00:00
 #SBATCH --mem=128G
 #SBATCH --cpus-per-task=64
 #SBATCH --output=watch_folder/%x-%j.out   # saves logs to logs/<jobname>-<jobid>.out
@@ -31,15 +31,15 @@ export SCRATCH_DIR='./scratch'
 srun python src/train.py   \
     experiment=training/single_system/fastflow_Ace-A-Nme \
     'model.teacher_ckpt_path=${paths.log_dir}/${task_name}/runs/tarflow_Ace-A-Nme_no_noise/checkpoints/last_weights_only.ckpt' \
-    'hydra.run.dir=${paths.log_dir}/${task_name}/runs/fastflow_Ace-A-Nme-dist_loss' \
-    +logger.wandb.name=fastflow_Ace-A-Nme-dist_loss \
+    'hydra.run.dir=${paths.log_dir}/${task_name}/runs/fastflow_Ace-A-Nme-dist_loss_kl' \
+    +logger.wandb.name=fastflow_Ace-A-Nme-dist_loss_kl \
     logger.wandb.project=fastflow   \
-    model.lambda_kl=0.0 \
-    model.kl_start_frac=1 model.align_until_frac=0.0 model.lambda_align=0 \
+    model.lambda_kl=0.01 \
+    model.kl_start_frac=0 model.align_until_frac=0.0 model.lambda_align=0 \
     model.noise_sigma=0.0   \
     trainer='ddp' trainer.strategy="ddp_find_unused_parameters_true" \
     trainer.max_epochs=3000 \
     'trainer.plugins=[]' \
-    +model.student_ckpt_path=scratch/transferable-samplers/logs/train/runs/fastflow_Ace-A-Nme-no-kl-no-noise-student-reverse-for-proposal-energy/checkpoints/last_weights_only.ckpt
+    +model.student_ckpt_path=scratch/transferable-samplers/logs/train/runs/fastflow_Ace-A-Nme-kl/checkpoints/last-v2_weights_only.ckpt
 
     # model.optimizer.lr=1e-3 \
